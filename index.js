4b1bcf00ed20e2f7385b2b0d20d56ed2f84544dd
Zachary Barnes <zachhsavage@gmail.com>
2023-03-30 10:19:25 -0400

    Added some basics to the index for inquirer, generatemarkdown, fs, and some questions

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+
+
+// Leave the License question for last, create some more types above
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE",
+            "GPL",
+            "BSD",
+            "None"
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
