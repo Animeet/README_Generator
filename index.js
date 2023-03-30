698d34783b6eb72ba26e5dbef5e2b8d72f30d217
Zachary Barnes <zachhsavage@gmail.com>
2023-03-30 16:50:03 -0400

    Added all of the prompts for the readme, and finished the layout of the README in generateMarkdown

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    // Title
     {
         type: "input",
-        message: "What is your GitHub username?",
-        name: "username"
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    // Description Section
+    {
+        type: "input",
+        message: "Provide a brief description of your project.",
+        name: "description"
     },
     {
         type: "input",
-        message: "What is your GitHub username?",
-        name: "username"
+        message: "What problem does your project solve?",
+        name: "problem"
+    },
+    {
+        type: "input",
+        message: "What did you learn from this project?",
+        name: "learn"
+    },
+    // Installation Section
+    {
+        type: "input",
+        message: "What command should be ran to install dependencies?",
+        name: "install"
+    },
+    // Usage Section
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    // Contribution Section
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the project?",
+        name: "contribute"
+    },
+    // Test Instructions Section
+    {
+        type: "input",
+        message: "What command should be ran to test the project?",
+        name: "test"
     },
 
-
-// Leave the License question for last, create some more types above
+    //License Section
     {
         type: "list",
         message: "What kind of license should your project have?",
@@ -29,17 +64,37 @@ const questions = [
             "BSD",
             "None"
         ]
-    }
+    },
+    //Questions Section
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub Email?",
+        name: "email"
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    var content = generateMarkdown(data);
+    fs.writeFile(fileName, content, function(error) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('success');
+    });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(questions).then(function (data) {
+        var fileName = 'README.md';
+        writeToFile(fileName, data);
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ** ${data.title} **
 
+  ## **Description**
+  - ${data.description}
+  - ${data.problem}
+  - ${data.learn}
+
+
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Contribution](#contribution)
+  4. [Testing](#testing)
+  5. [License](#license)
+  6. [Questions](#questions)
+
+  ---
+
+  ## ** Installation **
+  > ${data.install}
+
+  ---
+
+  ## ** Usage Information **
+  ${data.usage}
+
+
+  ---
+  
+  ## ** Contribution **
+  ${data.usage}
+
+
+  ---
+
+  ## ** Testing **
+  ${data.test}
+
+
+  ---
+
+  ## ** License **
+  License: ${data.license}
+
+  For more information on the license above, please go to [https://www.choosealicense.com/](https://www.choosealicense.com/).
+
+  ---
+
+  ## ** Questions **
+  If you have any questions about this repository, please contact me directly at [${data.email}](mailto:${data.email}).
+  You can find this repository, and others I have worked on at [${data.username}](https://www.github.com/${data.username}).
 `;
 }
 
 module.exports = generateMarkdown;
 
 
-// ${renderLicenseSection('MIT')}
-// You can/should add the above into the function for generateMarkdown
-// As well as some of the other functions for the licenses
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
